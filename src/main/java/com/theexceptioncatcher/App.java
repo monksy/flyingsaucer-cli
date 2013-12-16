7cee7c0bf8a8fc5e1a79f6e658d86307f0abcf99
Steven <crashoverride025@gmail.com>
2013-12-16 01:00:08 -0600

    Added javadoc, and checked to see if the input files existed.

--- src/main/java/com/theexceptioncatcher/App.java
+++ src/main/java/com/theexceptioncatcher/App.java
@@ -5,22 +5,35 @@ import com.lexicalscope.jewel.cli.CliFactory;
 import java.io.File;
 
 /**
- * Hello world!
- *
+ * This simple utiltiy is responsible for wrapping the XSLT/XML flying-saucer library into a command line utility. If
+ * this application succeeded the XML and XSL files will be transformed into the specified PDF file. The return code
+ * will yield a 0 result. If the process did not succeeded then the application will display an error, and yield an
+ * error code as the application's result.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         AppParameters inputs = CliFactory.parseArguments(AppParameters.class, args);
 
-        //TODO: Verify that the XSlT file exists
+        //Verify that the XSlT file exists
+        if (!doesFileExist(inputs.getXSLFile())) {
+            System.err.printf("Error: The XSL file '%s' does not exist.%n", inputs.getXSLFile());
+            System.exit(1);
+        }
 
-        //TODO: Verify that the XML File exists
+        //Verify that the XML File exists
+        if (!doesFileExist(inputs.getXmlFile())) {
+            System.err.printf("Error: The XML file '%s' does not exist.%n", inputs.getXmlFile());
+            System.exit(1);
+        }
 
         //TODO: Check to see if the overwrite is neccessary.
-           //TODO: Check to see if the output file exists if so then through an error
+        //TODO: Check to see if the output file exists if so then through an error
 
         //TODO: Perform transformation
+    }
+
+    private static boolean doesFileExist(String filePath) {
+        System.out.println(new File(filePath).getAbsolutePath());
+        return new File(filePath).isFile();
     }
 }
