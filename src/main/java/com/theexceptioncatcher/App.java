a3b01bb27c247453e9cc4e379f5851a7eb721ea0
Steven <crashoverride025@gmail.com>
2013-12-16 00:23:21 -0600

    Added parameter setup.

--- src/main/java/com/theexceptioncatcher/App.java
+++ src/main/java/com/theexceptioncatcher/App.java
@@ -1,5 +1,7 @@
 package com.theexceptioncatcher;
 
+import com.lexicalscope.jewel.cli.CliFactory;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        AppParameters inputs = CliFactory.parseArguments(AppParameters.class, args);
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/theexceptioncatcher/AppParameters.java
@@ -0,0 +1,20 @@
+package com.theexceptioncatcher;
+
+import com.lexicalscope.jewel.cli.Option;
+
+/**
+ * Created by steven on 12/15/13.
+ */
+public interface AppParameters {
+    @Option(longName = "xmlFile", description = "This parameter is the location of the XML file used in the " +
+            "transformation.")
+    String getXmlFile();
+
+    @Option(longName = "xslFile", description = "This parameter is the location of the XSLT file used in the " +
+            "transformation.")
+    String getXSLFile();
+
+    @Option(longName = "pdfOutput", description = "This parameter describes where the PDF formatted output will be " +
+            "stored.")
+    String getPDFOutput();
+}
