7a1967d4ef144617b1ada0c0e31d1004ea710449
Steven <crashoverride025@gmail.com>
2013-12-15 21:06:08 -0600

    Initial project items

---
+++ src/main/java/com/theexceptioncatcher/App.java
@@ -0,0 +1,13 @@
+package com.theexceptioncatcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
