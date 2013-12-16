961d29e8ebed80b9ddbf6dd45b2892909b0163a7
Steven <crashoverride025@gmail.com>
2013-12-16 00:39:36 -0600

    Wrote the tasks needed for this application.

--- src/main/java/com/theexceptioncatcher/App.java
+++ src/main/java/com/theexceptioncatcher/App.java
@@ -2,6 +2,8 @@ package com.theexceptioncatcher;
 
 import com.lexicalscope.jewel.cli.CliFactory;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -11,6 +13,14 @@ public class App
     public static void main( String[] args )
     {
         AppParameters inputs = CliFactory.parseArguments(AppParameters.class, args);
-        System.out.println( "Hello World!" );
+
+        //TODO: Verify that the XSlT file exists
+
+        //TODO: Verify that the XML File exists
+
+        //TODO: Check to see if the overwrite is neccessary.
+           //TODO: Check to see if the output file exists if so then through an error
+
+        //TODO: Perform transformation
     }
 }
